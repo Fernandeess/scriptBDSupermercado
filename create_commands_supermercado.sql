aeeca029da67d5789473ce676eea07df66f7a9b0
Fernandeess <matheus.rodrigues@sptech.school>
2023-06-14 08:10:59 -0300

    alteraçoes da sintaxe

--- create_commands_supermercado.sql
+++ create_commands_supermercado.sql
@@ -14,60 +14,92 @@ CREATE TABLE Endereco(
   txt_bairro varchar(100) NOT NULL,
   txt_cidade varchar(100) NOT NULL,
   txt_estado varchar(2) NOT NULL,
-  txt_cep char(8) NOT NULL,
-  nro_int_id_supermercado INT,
-  CONSTRAINT fk_endereco_supermercado FOREIGN KEY (nro_int_id_supermercado) REFERENCES Supermercado (nro_int_id_supermercado_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  nro_int_id_fornecedor INT,
-  CONSTRAINT fk_endereco_fornecedor FOREIGN KEY (nro_int_id_fornecedor) REFERENCES Fornecedor (nro_int_id_fornecedor_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_int_id_endereco_seq),
+  txt_cep char(9) NOT NULL,
+  nro_id_supermercado INT UNIQUE ,
+  CONSTRAINT fk_endereco_supermercado 
+	FOREIGN KEY (nro_id_supermercado) 
+	REFERENCES Supermercado (nro_int_id_supermercado_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  nro_id_fornecedor INT UNIQUE,
+  CONSTRAINT fk_endereco_fornecedor 
+	FOREIGN KEY (nro_id_fornecedor) 
+	REFERENCES Fornecedor (nro_int_id_fornecedor_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_endereco_seq)
 );
 
 -- Create table Supervisor
 CREATE TABLE Supervisor(
-  nro_id_supervisor_seq serial NOT NULL,
+  nro_int_id_supervisor_seq serial NOT NULL,
   txt_nome_supervisor varchar(100) NOT NULL,
   txt_cpf_supervisor char(11) NOT NULL,
   txt_email_supervisor varchar(100) NOT NULL,
   txt_telefone_supervisor char(11) NOT NULL,
-  nro_int_id_supermercado INT NOT NULL,
-  CONSTRAINT fk_supervisor_supermercado FOREIGN KEY (nro_int_id_supermercado) REFERENCES Supermercado (nro_int_id_supermercado_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_id_supervisor_seq)
+  nro_id_supermercado INT NOT NULL,
+  CONSTRAINT fk_supervisor_supermercado 
+	FOREIGN KEY (nro_id_supermercado) 
+	REFERENCES Supermercado (nro_int_id_supermercado_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_supervisor_seq)
 );
 
 -- Create table Funcionario
 CREATE TABLE Funcionario(
-  nro_id_funcionario_seq serial NOT NULL,
+  nro_int_id_funcionario_seq serial NOT NULL,
   txt_nome_funcionario varchar(100) NOT NULL,
   txt_cpf_funcionario char(11) NOT NULL,
   txt_telefone_funcionario char(11) NOT NULL,
   txt_email_funcionario varchar(100) NOT NULL,
-  nro_id_int_supervisor INT NOT NULL,
-  nro_int_id_supermercado INT NOT NULL,
-  CONSTRAINT fk_funcionario_supermercado FOREIGN KEY (nro_int_id_supermercado) REFERENCES Supermercado (nro_int_id_supermercado_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_funcionario_supervisor FOREIGN KEY (nro_id_int_supervisor) REFERENCES Supervisor (nro_id_supervisor_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_id_funcionario_seq)
+  nro_id_supervisor INT NOT NULL,
+  nro_id_supermercado INT NOT NULL,
+  CONSTRAINT fk_funcionario_supermercado 
+	FOREIGN KEY (nro_id_supermercado) 
+	REFERENCES Supermercado (nro_int_id_supermercado_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  CONSTRAINT fk_funcionario_supervisor 
+	FOREIGN KEY (nro_id_supervisor) 
+	REFERENCES Supervisor (nro_int_id_supervisor_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_funcionario_seq)
 );
 
 -- Create table Caixa
 CREATE TABLE Caixa (
   nro_int_id_caixa_seq serial NOT NULL,
   nro_tipo_caixa INT NOT NULL,
-  nro_int_id_supermercado INT NOT NULL,
-  CONSTRAINT fk_caixa_supermercado FOREIGN KEY (nro_int_id_supermercado) REFERENCES Supermercado (nro_int_id_supermercado_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_id_caixa_seq)
+  nro_id_supermercado INT NOT NULL,
+  CONSTRAINT fk_caixa_supermercado 
+	FOREIGN KEY (nro_id_supermercado) 
+	REFERENCES Supermercado (nro_int_id_supermercado_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_caixa_seq)
 );
 
 -- Create table turno_caixa
 CREATE TABLE Turno_caixa(
-  nro_id_turno_caixa_seq serial NOT NULL,
+  nro_int_id_turno_caixa_seq serial NOT NULL,
   dt_date_time_abertura TIMESTAMP NOT NULL,
   dt_date_time_fechamento TIMESTAMP,
   nro_saldo_caixa DECIMAL(10,2) NOT NULL,
   nro_id_caixa INT NOT NULL,
   nro_id_funcionario INT NOT NULL,
-  CONSTRAINT fk_turno_caixa_caixa FOREIGN KEY (nro_id_caixa) REFERENCES Caixa (nro_id_caixa_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_turno_caixa_funcionario FOREIGN KEY (nro_id_funcionario) REFERENCES Funcionario (nro_id_funcionario_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_id_turno_caixa_seq)
+  CONSTRAINT fk_turno_caixa_caixa 
+	FOREIGN KEY (nro_id_caixa) 
+	REFERENCES Caixa (nro_int_id_caixa_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  CONSTRAINT fk_turno_caixa_funcionario 
+	FOREIGN KEY (nro_id_funcionario) 
+	REFERENCES Funcionario (nro_int_id_funcionario_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_turno_caixa_seq)
 );
 
 -- Create table Cliente
@@ -84,13 +116,23 @@ CREATE TABLE Categoria (
   txt_nome_categoria varchar(255) NOT NULL,
   PRIMARY KEY (nro_int_id_categoria_seq)
 );
+-- Create table Lote
+CREATE TABLE Lote (
+  nro_int_id_lote_seq serial NOT NULL,
+  nro_id_fornecedor INT NOT NULL,
+  nro_preco_lote DECIMAL(10,2) NOT NULL,
+  CONSTRAINT fk_lote_fornecedor 
+	FOREIGN KEY (nro_id_fornecedor) 
+	REFERENCES Fornecedor (nro_int_id_fornecedor_seq)
+  PRIMARY KEY (nro_int_id_lote_seq)
+);
 
 -- Create table Pacote
 CREATE TABLE Pacote (
   nro_int_id_pacote_seq serial NOT NULL,
   txt_nome_pacote varchar(255) NOT NULL,
   nro_desconto_pacote DECIMAL(10,2) NOT NULL,
-  nro_qntd_itens INT NOT NULL
+  nro_qntd_itens INT NOT NULL,
   PRIMARY KEY (nro_int_id_pacote_seq)
 );
 
@@ -101,41 +143,88 @@ CREATE TABLE Produto (
   nro_preco_produto DECIMAL(10,2) NOT NULL,
   nro_int_id_pacote INT,
   nro_int_id_categoria INT NOT NULL,
-  CONSTRAINT fk_produto_categoria FOREIGN KEY (nro_int_id_categoria) REFERENCES Categoria (nro_int_id_categoria_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_produto_pacote FOREIGN KEY (nro_int_id_pacote) REFERENCES Pacote (nro_int_id_pacote_seq) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT fk_produto_categoria 
+	FOREIGN KEY (nro_int_id_categoria) 
+	REFERENCES Categoria (nro_int_id_categoria_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  CONSTRAINT fk_produto_pacote 
+	FOREIGN KEY (nro_int_id_pacote) 
+	REFERENCES Pacote (nro_int_id_pacote_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
   PRIMARY KEY (nro_int_id_produto_seq)
 );
 
+
+
 -- Create table Estoque
 CREATE TABLE Estoque_produto (
-  nro_int_id_estoque_seq serial NOT NULL,
-  nro_int_id_produto INT NOT NULL,
+  nro_int_id_estoque_produto_seq serial NOT NULL,
+  nro_id_produto INT NOT NULL,
+  nro_id_lote INT NOT NULL,
   nro_qtd_produto INT NOT NULL,
-  CONSTRAINT fk_estoque_produto FOREIGN KEY (nro_int_id_produto) REFERENCES Produto (nro_int_id_produto_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_int_id_estoque_seq)
+  CONSTRAINT fk_estoque_produto 
+	FOREIGN KEY (nro_id_produto) 
+	REFERENCES Produto (nro_int_id_produto_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+	CONSTRAINT fk_estoque_lote 
+	FOREIGN KEY (nro_id_lote) 
+	REFERENCES Lote (nro_int_id_lote_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_estoque_produto_seq)
 );
 
--- Create table Compra
-CREATE TABLE Compra (
-  nro_int_id_compra_seq serial NOT NULL,
-  nro_int_id_produto INT NOT NULL,
-  nro_int_id_cliente INT NOT NULL,
-  nro_int_id_caixa INT NOT NULL,
-  dt_date_time_compra TIMESTAMP NOT NULL,
-  CONSTRAINT fk_compra_produto FOREIGN KEY (nro_int_id_produto) REFERENCES Produto (nro_int_id_produto_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_compra_cliente FOREIGN KEY (nro_int_id_cliente) REFERENCES Cliente (nro_int_id_cliente_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_compra_caixa FOREIGN KEY (nro_int_id_caixa) REFERENCES Caixa (nro_id_caixa_seq) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY (nro_int_id_compra_seq)
+-- Create table Venda
+CREATE TABLE Venda (
+  nro_int_id_venda_seq serial NOT NULL,
+  nro_id_cliente INT NOT NULL,
+  nro_id_nota_fiscal INT NOT NULL,
+  dt_date_time_venda TIMESTAMP NOT NULL,
+  CONSTRAINT fk_compra_cliente 
+	FOREIGN KEY (nro_id_cliente) 
+	REFERENCES Cliente (nro_int_id_cliente_seq) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+	CONSTRAINT fk_compra_nota_fiscal 
+	FOREIGN KEY (nro_id_nota_fiscal) 
+	REFERENCES Nota_fiscal (nro_int_id_nota_fiscal_seq) 
+  ON DELETE CASCADE
+  ON UPDATE CASCADE,
+  PRIMARY KEY (nro_int_id_venda_seq)
+);
+
+CREATE TABLE Itens_venda (
+	nro_int_id_itens_venda_seq SERIAL NOT NULL,
+	nro_id_estoque_produto INT NOT NULL,
+	nro_id_venda INT NOT NULL,
+	nro_qntd_itens INT NOT NULL,
+CONSTRAINT fk_itens_venda_id_estoque_produto 
+	FOREIGN KEY (nro_id_estoque_produto) 
+	REFERENCES Estoque_produto (nro_int_id_estoque_produto_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+CONSTRAINT fk_itens_venda_id_venda 
+	FOREIGN KEY (nro_id_venda) 
+	REFERENCES Venda (nro_int_id_venda_seq) 
+	ON DELETE CASCADE 
+	ON UPDATE CASCADE,
+	PRIMARY KEY (nro_int_id_itens_venda_seq)
 );
 
 -- Create table Nota_fiscal
+DROP TABLE Nota_fiscal;
 CREATE TABLE Nota_fiscal(
-  nro_int_id_nota_fiscal INT,
+  nro_int_id_nota_fiscal_seq SERIAL,
   nro_int_id_turno_caixa INT NOT NULL,
   nro_int_price_total DECIMAL(10,2) NOT NULL,
   dt_date_time_nota_fiscal TIMESTAMP,
-  CONSTRAINT fk_nota_fiscal_turno_caixa FOREIGN KEY (nro_int_id_turno_caixa) REFERENCES turno_caixa (nro_id_turno_caixa_seq),
-  PRIMARY KEY (nro_int_id_nota_fiscal)
+  CONSTRAINT fk_nota_fiscal_turno_caixa 
+	FOREIGN KEY (nro_int_id_turno_caixa) 
+	REFERENCES turno_caixa (nro_int_id_turno_caixa_seq),
+  PRIMARY KEY (nro_int_id_nota_fiscal_seq)
 );
 
 -- Create table Fornecedor
@@ -148,13 +237,3 @@ CREATE TABLE Fornecedor (
   PRIMARY KEY (nro_int_id_fornecedor_seq)
 );
 
--- Create table Lote
-CREATE TABLE Lote (
-  nro_int_id_lote_seq serial NOT NULL,
-  nro_id_fornecedor INT NOT NULL,
-  nro_int_id_produto INT NOT NULL,
-  nro_preco_lote DECIMAL(10,2) NOT NULL,
-  CONSTRAINT fk_lote_fornecedor FOREIGN KEY (nro_id_fornecedor) REFERENCES Fornecedor (nro_int_id_fornecedor_seq),
-  CONSTRAINT fk_lote_produto FOREIGN KEY (nro_int_id_produto) REFERENCES Produto (nro_int_id_produto_seq),
-  PRIMARY KEY (nro_int_id_lote_seq)
-);
--- insert_commands_supermercado.sql
+++ insert_commands_supermercado.sql
@@ -3,25 +3,29 @@ INSERT INTO Supermercado (txt_nome_supermercado, txt_cnpj_supermercado)
 VALUES ('Carrefour', '12345678901234'),
 ('Extra', '98765432109876');
 
+INSERT INTO Endereco (txt_rua, txt_numero, txt_bairro, txt_cidade, txt_estado, txt_cep, nro_id_fornecedor)
+VALUES ('Rua A', '123', 'Bairro A', 'Cidade A', 'AA', '12345678', 1),
+('Rua B', '456', 'Bairro B', 'Cidade B', 'BB', '87654321', 2);
+
 INSERT INTO Endereco (txt_rua, txt_numero, txt_bairro, txt_cidade, txt_estado, txt_cep, nro_int_id_supermercado)
 VALUES ('Rua A', '123', 'Bairro A', 'Cidade A', 'AA', '12345678', 1),
 ('Rua B', '456', 'Bairro B', 'Cidade B', 'BB', '87654321', 2);
 
-INSERT INTO Supervisor (txt_nome_supervisor, txt_cpf_supervisor, txt_telefone_supervisor, txt_email_supervisor, nro_int_id_supermercado)
+INSERT INTO Supervisor (txt_nome_supervisor, txt_cpf_supervisor, txt_telefone_supervisor, txt_email_supervisor, nro_id_supermercado)
 VALUES ('Paulo matias', '12345678901', '987654321', 'paulomatias@example.com', 1),
 ('Ana Paula', '09876543210', '123456789', 'anaPaula@example.com', 2);
 
-INSERT INTO Funcionario (txt_nome_funcionario, txt_cpf_funcionario, txt_telefone_funcionario, txt_email_funcionario, nro_id_int_supervisor, nro_int_id_supermercado)
+INSERT INTO Funcionario (txt_nome_funcionario, txt_cpf_funcionario, txt_telefone_funcionario, txt_email_funcionario, nro_id_supervisor, nro_id_supermercado)
 VALUES ('Jorge Lucas', '11111111111', '111111111', 'funcionarioA@example.com', 1, 1),
 ('Pedro loots', '22222222222', '222222222', 'funcionarioB@example.com', 2, 2);
 
-INSERT INTO Caixa (nro_tipo_caixa, nro_int_id_supermercado)
+INSERT INTO Caixa (nro_tipo_caixa, nro_id_supermercado)
 VALUES (1, 1),
 (2, 2);
 
-INSERT INTO turno_caixa (dt_date_time_abertura, dt_date_time_fechamento, nro_salario_caixa, nro_id_caixa, nro_id_funcionario)
-VALUES ('2023-06-09 09:00:00', '2023-06-09 17:00:00', 1000.00, 1, 1),
-('2023-06-09 10:00:00', '2023-06-09 18:00:00', 1200.00, 2, 2);
+INSERT INTO turno_caixa (dt_date_time_abertura, dt_date_time_fechamento, nro_saldo_caixa, nro_id_caixa, nro_id_funcionario)
+VALUES ('2023-06-09 09:00:00', null, 0.00, 1, 1),
+('2023-06-09 10:00:00', null, 0000.00, 2, 2);
 
 INSERT INTO Cliente (txt_nome_cliente, txt_cpf_cliente)
 VALUES ('Matheus', '33333333333'),
@@ -33,29 +37,25 @@ VALUES ('Bebidas'),
        ('Limpeza'),
        ('Higiene'),
        ('Eletrônicos');
+SELECT * FROM Pacote;
 
+INSERT INTO Pacote (txt_nome_pacote,nro_qntd_itens, nro_desconto_pacote)
+VALUES ('Fardo Skol',6, 0.4),
+('Fardo Brahma',6, 0.3),
+('Fardo Guaraná',6, 0.2),
+('Fardo Pepsi',6, 0.2),
+('Fardo Coca-Cola',6, 0.3);
 
-INSERT INTO Pacote (txt_nome_pacote, nro_desconto_pacote)
-VALUES ('Fardo Skol', 0.4),
-('Fardo Brahma', 0.3),
-('Fardo Guaraná', 0.2),
-('Fardo Pepsi', 0.2),
-('Fardo Coca-Cola', 0.3);
+INSERT INTO Produto (txt_nome_produto, nro_preco_produto, nro_id_pacote, nro_id_categoria)
+VALUES ('Skol', 3.00, 3, 1),
+('Brahma', 2.50, 4, 2),
+('Guaraná', 4.00, 5, 1),
+('Pepsi', 3.00, 6, 1),
+('Coca-Cola', 5.00, 7, 1);
 
-INSERT INTO Produto (txt_nome_produto, dt_validade_produto, nro_preco_produto, nro_int_id_pacote, nro_int_id_categoria)
-VALUES ('Skol', '2023-12-31', 3.00, 1, 1),
-('Brahma', '2023-12-31', 2.50, 2, 2),
-('Guaraná', '2023-12-31', 4.00, 3, 1),
-('Pepsi', '2023-12-31', 3.00, 4, 1),
-('Coca-Cola', '2023-12-31', 5.00, 5, 1);
+INSERT INTO Lote (nro_id_fornecedor,nro_int_id_produto,nro_preco_lote) VALUES 
+(1,5,  )
 
-INSERT INTO Estoque (nro_int_id_produto, nro_qtd_produto)
-VALUES (1, 100),
-(2, 200);
-
-INSERT INTO Compra (nro_int_id_produto, nro_int_id_cliente, nro_int_id_caixa, dt_date_time_compra)
-VALUES (1, 1, 1, '2023-06-09 10:30:00'),
-(2, 2, 2, '2023-06-09 11:30:00');
 
 INSERT INTO Nota_fiscal (nro_int_id_nota_fiscal, nro_int_id_turno_caixa, nro_int_price_total, dt_date_time_nota_fiscal)
 VALUES (1, 1, 10.00, '2023-06-09 10:30:00'),
@@ -65,6 +65,8 @@ INSERT INTO Fornecedor (txt_razao_social_fornecedor, txt_cnpj_fornecedor, txt_te
 VALUES ('Fornecedor A', '12345678901234', '111111111', 'fornecedorA@example.com'),
 ('Fornecedor B', '98765432109876', '222222222', 'fornecedorB@example.com');
 
+SELECT * FROM Fornecedor
+
 INSERT INTO Lote (nro_id_fornecedor, nro_int_id_produto, nro_preco_lote)
-VALUES (1, 1, 50.00),
-(2, 2, 70.00);
+VALUES (1, 1, 1500.00),
+(2, 2, 170.00);
